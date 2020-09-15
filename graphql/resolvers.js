3fd583f76f0f62d6667c2e1faf30d1abb70f44a0
patrick <patrick204nqh@gmail.com>
2020-09-15 14:54:23 +0700

    mutation create User

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -11,4 +11,27 @@ module.exports = {
       }
     }
   },
+  Mutation: {
+    register: async (_, args) => {
+      const { username, email, password, confirmPassword } = args;
+
+      try {
+        // TODO: Validate input data
+
+        // TODO: Check if username / email exists
+
+        // TODO: Hash password
+
+        // TODO: Create user
+        const user = await User.create({
+          username, email, password
+        })
+        // TODO: Return user
+        return user;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    }
+  }
 };
