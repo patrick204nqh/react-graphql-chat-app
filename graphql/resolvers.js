1a8f0a0bee54fa78ae7df01d86dfe602220c204e
patrick <patrick204nqh@gmail.com>
2020-09-16 13:36:48 +0700

    query login & validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 
 const { User } = require('../models');
 
@@ -11,6 +11,40 @@ module.exports = {
         return users
       } catch (err) {
         console.error(err);
+      }
+    },
+    login: async (_, args) => {
+      const { username, password } = args;
+      let errors = {};
+
+      try {
+        if (username.trim() === '') errors.username = 'username must not be empty';
+        if (password.trim() === '') errors.password = 'password must not be empty';
+
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError('bad input', { errors });
+        }
+
+        const user = await User.findOne({
+          where: { username }
+        })
+
+        if (!user) {
+          errors.username = 'User not found';
+          throw new UserInputError('user not found', { errors });
+        }
+
+        const correctPassword = await bcrypt.compare(password, user.password);
+
+        if (!correctPassword) {
+          errors.password = 'Password is incorrect';
+          throw new AuthenticationError('password is incorrect', { errors });
+        }
+
+        return user;
+      } catch (err) {
+        console.log(err);
+        throw err
       }
     }
   },
