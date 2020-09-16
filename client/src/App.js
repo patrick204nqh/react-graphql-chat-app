29283615f4c401447814bc7030ba14fb050cf7db
patrick <patrick204nqh@gmail.com>
2020-09-17 00:46:10 +0700

    setup apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 
+import ApolloProvider from './ApolloProvider';
+
 import './App.scss';
 import Register from './pages/Register';
 
 function App() {
   return (
-    <Container className="pt-5">
-      <Register />
-    </Container>
+    <ApolloProvider>
+      <Container className="pt-5">
+        <Register />
+      </Container>
+    </ApolloProvider>
   )
 }
 
