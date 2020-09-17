83313ed00bbc84bac15e501019375911c97d22a7
patrick <patrick204nqh@gmail.com>
2020-09-18 02:39:14 +0700

    expires token

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,7 +1,21 @@
 import React, { createContext, useReducer, useContext } from 'react';
+import jwtDecode from 'jwt-decode';
 
 const AuthStateContext = createContext();
 const AuthDispatchContext = createContext();
+
+let user;
+const token = localStorage.getItem('token');
+if (token) {
+  const decodedToken = jwtDecode(token);
+  const expiresAt = new Date(decodedToken.exp * 1000)
+
+  if (new Date() > expiresAt) {
+    localStorage.removeItem('token');
+  } else {
+    user = decodedToken
+  }
+} else console.log('No token found')
 
 const authReducer = (state, action) => {
   switch (action.type) {
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Col, Row } from 'react-bootstrap'
+import { Button, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 import { useAuthDispatch } from '../context/auth';
