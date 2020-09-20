31a8579d4117c7d251f0d1252bb826b0ee334273
patrick <patrick204nqh@gmail.com>
2020-09-21 02:28:04 +0700

    get messages with useLazyQuery

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,49 @@
-import React from 'react'
-import { Button, Row, Col } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Button, Row, Col, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useLazyQuery } from '@apollo/client'
 
 import { useAuthDispatch } from '../context/auth';
 
 const GET_USERS = gql`
   query getUsers {
     getUsers {
-      username email createdAt
+      username createdAt imageUrl
+      latestMessage {
+        uuid from to content createdAt
+      }
+    }
+  }
+`;
+
+const GET_MESSAGES = gql`
+  query getMessages($from: String!) {
+      getMessages(from: $from) {
+        uuid from to content createdAt
     }
   }
 `;
 
 export default function Home({ history }) {
   const dispatch = useAuthDispatch();
+  const [selectedUser, setSelectedUser] = useState(null)
 
   const logout = () => {
     dispatch({ type: 'LOGOUT' })
     history.push('/login')
   }
 
+  const [getMessages, { loading: messagesLoading, data: messagesData }] = useLazyQuery(GET_MESSAGES);
+
+  useEffect(() => {
+    if (selectedUser) {
+      getMessages({ variables: { from: selectedUser } })
+    }
+  }, [selectedUser]);
+
+  if (messagesData) console.log(messagesData.getMessages)
+
   const { loading, data, error } = useQuery(GET_USERS)
-
-  if (error) {
-    console.log(error)
-  }
-
-  if (data) {
-    console.log(data)
-  }
 
   let usersMarkup;
   if (!data || loading) {
@@ -38,9 +52,28 @@ export default function Home({ history }) {
     usersMarkup = <p>no users have joined yet </p>
   } else if (data.getUsers.length > 0) {
     usersMarkup = data.getUsers.map(user => (
-      <div key={user.username}>
+      <div
+        className="d-flex p-3"
+        key={user.username}
+        onClick={() => setSelectedUser(user.username)}
+      >
+        <Image
+          src={user.imageUrl}
+          roundedCircle
+          className="mr-2"
+          style={{
+            width: 50,
+            height: 50,
+            objectFit: 'cover'
+          }} />
+        <div>
+          <p className="text-success">{user.username}</p>
+          <p className="font-weight-light">
+            {user.latestMessage ? user.latestMessage.content : 'You are now connected!'}
+          </p>
+        </div>
         <p>{user.username}</p>
-      </div>
+      </div >
     ))
   }
 
@@ -56,7 +89,7 @@ export default function Home({ history }) {
         <Button variant="link" onClick={logout}>Logout</Button>
       </Row>
       <Row className="bg-white">
-        <Col xs={4}>
+        <Col xs={4} className="p-0 bg-secondary">
           {usersMarkup}
         </Col>
         <Col xs={8}>
