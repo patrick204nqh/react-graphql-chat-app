7eec509e3888c3c9f45cd26b8a9781ab7b706f74
patrick <patrick204nqh@gmail.com>
2020-09-21 13:02:36 +0700

    setup message context

--- client/src/App.js
+++ client/src/App.js
@@ -11,21 +11,24 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 
 import { AuthProvider } from './context/auth';
+import { MessageProvider } from './context/message';
 import DynamicRoute from './util/DynamicRoute';
 
 function App() {
   return (
     <ApolloProvider>
       <AuthProvider>
-        <Router>
-          <Container className="pt-5">
-            <Switch>
-              <DynamicRoute exact path="/" component={Home} authenticated />
-              <DynamicRoute path="/login" component={Login} guest />
-              <DynamicRoute path="/register" component={Register} guest />
-            </Switch>
-          </Container>
-        </Router>
+        <MessageProvider>
+          <Router>
+            <Container className="pt-5">
+              <Switch>
+                <DynamicRoute exact path="/" component={Home} authenticated />
+                <DynamicRoute path="/login" component={Login} guest />
+                <DynamicRoute path="/register" component={Register} guest />
+              </Switch>
+            </Container>
+          </Router>
+        </MessageProvider>
       </AuthProvider>
     </ApolloProvider>
   )
