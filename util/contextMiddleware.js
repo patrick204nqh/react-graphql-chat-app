fcf081bdd7ad49b327ef148cc671bee20fcb0faf
patrick <patrick204nqh@gmail.com>
2020-09-23 00:47:18 +0700

    context Authorization

--- util/contextMiddleware.js
+++ util/contextMiddleware.js
@@ -3,11 +3,14 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config/env.json');
 
 module.exports = (context) => {
+  let token
   if (context.req && context.req.headers.authorization) {
-    const token = context.req.headers.authorization.split('Bearer ')[1];
-    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-      context.user = decodedToken;
-    })
+    token = context.req.headers.authorization.split('Bearer ')[1];
+  } else if (context.connection && context.connection.context.Authorization) {
+    token = context.connection.context.Authorization
   }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    context.user = decodedToken;
+  })
   return context;
 }
