c1867b6f8d76eac31c3e54573b97ef07eab63731
patrick <patrick204nqh@gmail.com>
2020-09-23 13:56:19 +0700

    reactions table && handle

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,19 @@
 const userResolvers = require('./users');
 const messageResolvers = require('./messages');
 
+const { User, Message } = require('../../models');
+
 module.exports = {
   Message: {
     createdAt: (parent) => parent.createdAt.toISOString()
   },
   User: {
     createdAt: (parent) => parent.createdAt.toISOString()
+  },
+  Reaction: {
+    createdAt: (parent) => parent.createdAt.toISOString(),
+    Message: async (parent) => await Message.findByPk(parent.messageId),
+    User: async (parent) => await User.findByPk(parent.userId),
   },
   Query: {
     ...userResolvers.Query,
--- graphql/resolvers/messages.js
+++ graphql/resolvers/messages.js
@@ -1,7 +1,12 @@
-const { UserInputError, AuthenticationError, withFilter } = require('apollo-server');
+const {
+  UserInputError,
+  AuthenticationError,
+  ForbiddenError,
+  withFilter
+} = require('apollo-server');
 const { Op } = require('sequelize');
 
-const { User, Message } = require('../../models');
+const { User, Message, Reaction } = require('../../models');
 
 module.exports = {
   Query: {
@@ -60,6 +65,50 @@ module.exports = {
         return message;
       } catch (err) {
         throw err;
+      }
+    },
+    reactToMessage: async (_, { uuid, content }, { user }) => {
+      const reactions = ['❤️', '😆', '😯', '😢', '😡', '👍', '👎']
+
+      try {
+        // Validate reaction content
+        if (!reactions.includes(content)) {
+          throw new UserInputError('Invalid reaction')
+        }
+
+        // Get user
+        const username = user ? user.username : ''
+        user = await User.findOne({ where: { username } })
+        if (!user) throw new AuthenticationError('Unauthenticated')
+
+        // Get message
+        const message = await Message.findOne({ where: { uuid } })
+        if (!message) throw new UserInputError('message not found')
+
+        if (message.from !== user.username && message.to !== user.username) {
+          throw new ForbiddenError('Unauthorized')
+        }
+
+        let reaction = await Reaction.findOne({
+          where: { messageId: message.id, userId: user.id }
+        })
+
+        if (reaction) {
+          // Reaction exists, update it
+          reaction.content = content
+          await reaction.save()
+        } else {
+          // Reaction doesn't exists, create it
+          reaction = await Reaction.create({
+            messageId: message.id,
+            userId: user.id,
+            content
+          })
+        }
+
+        return reaction
+      } catch (err) {
+        throw err
       }
     }
   },
